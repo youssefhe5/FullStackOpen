d09a66d7753edb9d12b31618e2a4a5d802ac0ef0
youssefhe5 <youssefhe5@yahoo.com>
2023-03-06 19:13:34 -0500

    Adding 'The whole app to the internet'

--- part3/part3-notes(back-end)/index.js
+++ part3/part3-notes(back-end)/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method: ', request.method);
@@ -89,6 +93,7 @@ let notes = [
   
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
