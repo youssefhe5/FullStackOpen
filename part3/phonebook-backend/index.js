fe8fa54f06fe473baa7a0f1fc7f126a207e8755f
youssefhe5 <youssefhe5@yahoo.com>
2023-03-09 07:12:00 -0500

    Finished 3.11

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -9,6 +9,7 @@ morgan.token("body", (req) => {
   return JSON.stringify(req.body);
 });
 app.use(morgan(":method :url :status - :response-time ms :body"));
+app.use(express.static("build"));
 
 let persons = [
   {
