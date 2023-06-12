c4a540589b92d1f31c5494300bdb7bc9c6d9fb79
youssefhe5 <youssefhe5@yahoo.com>
2023-06-11 20:50:42 -0400

    Finished part3-4

--- part3/part3-notes(back-end)/index.js
+++ part3/part3-notes(back-end)/index.js
@@ -1,99 +1,78 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Note = require("./models/note");
 
-app.use(cors())
-app.use(express.static('build'))
+app.use(cors());
+app.use(express.static("build"));
 
 const requestLogger = (request, response, next) => {
-  console.log('Method: ', request.method);
-  console.log('Path:   ', request.path);
-  console.log('Body:   ', request.body);
-  console.log('---');
-  next()
-}
+  console.log("Method: ", request.method);
+  console.log("Path:   ", request.path);
+  console.log("Body:   ", request.body);
+  console.log("---");
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(express.json())
-app.use(requestLogger)
+app.use(express.json());
+app.use(requestLogger);
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-  })
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-  app.get('/api/notes', (request, response) => {
-    response.json(notes)
-  })
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id).then((note) => {
+    response.json(note);
+  });
+});
 
-  app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
 
-    if (note) {
-        response.json(note)
-    } else {
-        response.status(404).end()
-    }
-  })
+  response.status(204).end();
+});
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
 
-    response.status(204).end()
-  })
+  return maxId + 1;
+};
 
-  const generateId = () => {
-    const maxId =  notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) : 0
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
-    return maxId + 1
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-  app.post('/api/notes', (request, response) => {
-    const body = request.body
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
+app.use(unknownEndpoint);
 
-    notes = notes.concat(note)
-    
-    response.json(note)
-  })
-  
-app.use(unknownEndpoint)
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
--- part3/part3-notes(back-end)/mongo.js
+++ part3/part3-notes(back-end)/mongo.js
@@ -1,31 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url =
-  `mongodb+srv://youssefhe5:${password}@cluster0.kvdorne.mongodb.net/noteApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://youssefhe5:${password}@cluster0.kvdorne.mongodb.net/noteApp?retryWrites=true&w=majority`;
 
-mongoose.set('strictQuery',false)
-mongoose.connect(url)
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model("Note", noteSchema);
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
-  })
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
 
 // const note = new Note({
 //   content: 'HTML is Easy',
